54672c4b167e5f5fbcca7198388a37c176f42d50
andrews-zack <zachary.andrews.au19@gmail.com>
2022-10-19 18:31:09 +0000

    first look at wireframe

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -4,7 +4,17 @@ function Footer() {
     return(
         <div className="container">
             <div className="row">
-                <div className="col-2"></div>
+                <div className="col-2">
+                    <p>Count</p>
+                </div>
+                <div className="col-6">
+                    <button className="btn btn-success">All Tasks</button>
+                    <button className="btn btn-success">Active Tasks</button>
+                    <button className="btn btn-success">Completed Tasks</button>
+                </div>
+                <div className="col-2">
+                    <button className="btn btn-primary">Complete all tasks</button>
+                </div>
             </div>
         </div>
     )
--- app/src/components/List.js
+++ app/src/components/List.js
@@ -1,7 +1,22 @@
 
 
 function List() {
-
+    return(
+        <ul class="list-group">
+            <li class="list-group-item">
+                <input class="form-check-input me-1" type="checkbox" value="" id="firstCheckbox"></input>
+                <label class="form-check-label" for="firstCheckbox">Take out the trash</label>
+            </li>
+            <li class="list-group-item">
+                <input class="form-check-input me-1" type="checkbox" value="" id="secondCheckbox"></input>
+                <label class="form-check-label" for="secondCheckbox">Clean dishes</label>
+            </li>
+            <li class="list-group-item">
+                <input class="form-check-input me-1" type="checkbox" value="" id="thirdCheckbox"></input>
+                <label class="form-check-label" for="thirdCheckbox">Do laundry</label>
+            </li>
+        </ul>
+    )
 }
 
 export default List
