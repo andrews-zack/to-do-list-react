b51e7c2cf639242a9ae55b21af74ab5527427db8
andrews-zack <zachary.andrews.au19@gmail.com>
2022-10-19 21:21:20 +0000

    finishing touches on wireframe

--- app/src/components/Footer.js
+++ app/src/components/Footer.js
@@ -12,7 +12,7 @@ function Footer() {
                     <button className="btn btn-info mx-1 my-1">Active Tasks</button>
                     <button className="btn btn-info mx-1 my-1">Completed Tasks</button>
                 </div>
-                <div className="col-auto">
+                <div className="col-auto ms-lg-4">
                     <button className="btn btn-success my-1 mx-1">✓ all</button>
                     <button className="btn btn-danger my-1 mx-1">X all</button>
                 </div>
