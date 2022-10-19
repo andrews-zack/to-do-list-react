49f4264bc562872ca034cae5dd042abfefce61aa
andrews-zack <zachary.andrews.au19@gmail.com>
2022-10-19 18:11:38 +0000

    initial commit, set up file structure

---
+++ app/src/components/Footer.js
@@ -0,0 +1,11 @@
+
+
+function Footer() {
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-2"></div>
+            </div>
+        </div>
+    )
+}
---
+++ app/src/components/Input.js
@@ -0,0 +1,12 @@
+
+
+function Input() {
+    return (
+        <div className="input-group mb-3">
+            <input type="text" className="form-control" placeholder="To-do..." aria-label="To-do list input" aria-describedby="button-addon2"></input>
+            <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+        </div>
+    )
+}
+
+export default Input
